c60b115deaf2b0322c34adbba326a00b8ed95827
Ichsanudin <ichsanudin.chairin@bluebirdgroup.com>
2024-08-05 18:38:56 +0700

    Second commit

---
+++ app/src/main/java/com/hirin/cryptofeed/presentation/list/CryptoListFragment.kt
@@ -0,0 +1,32 @@
+package com.hirin.cryptofeed.presentation.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import com.hirin.cryptofeed.R
+
+class CryptoListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {}
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(inflater.context).apply {
+        layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
+        setContent {
+            CryptoListScreen()
+        }
+    }
+}
